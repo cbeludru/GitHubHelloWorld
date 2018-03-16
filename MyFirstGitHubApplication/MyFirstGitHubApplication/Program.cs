12732e320b3adef14b737bb634f9d4d2e714beed
cbeludru <chandrasekhar.beluduru@gmail.com>
2018-03-16 10:28:05 +0530

    sample changes

--- MyFirstGitHubApplication/MyFirstGitHubApplication/Program.cs
+++ MyFirstGitHubApplication/MyFirstGitHubApplication/Program.cs
@@ -7,6 +7,8 @@ namespace MyFirstGitHubApplication
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("Hello test project 2!");
         }
     }
 }
